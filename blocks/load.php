d64ca4274ab346d6b16b1dfa0e05fb7b46480df5
luisavilhena <luisavilhena@gmail.com>
2022-09-13 17:56:23 -0300

    erro

--- blocks/load.php
+++ blocks/load.php
@@ -1,25 +1,25 @@
 <?php
-	// require('banner.php');
-	// require('text-topics.php');
-	// require('carousel.php');
-	// require('logo-carousel.php');
-	// require('equipe.php');
-	// require('text-items.php');
-	// require('services.php');
-	// require('carousel-description.php');
-	// require('cards-list.php');
-	// require('mail.php');
-	// require('img-text.php');
-	// require('link-list.php');
-	// require('store.php');
+	require('banner.php');
+	require('text-topics.php');
+	require('carousel.php');
+	require('logo-carousel.php');
+	require('equipe.php');
+	require('text-items.php');
+	require('services.php');
+	require('carousel-description.php');
+	require('cards-list.php');
+	require('mail.php');
+	require('img-text.php');
+	require('link-list.php');
+	require('store.php');
 
 
-	require('rich-text.php');
-	require('image-columns.php');
-	require('one-image.php');
-	require('text-box.php');
-	require('social-media.php');
-	require('columns-image-text-box.php');
-	require('columns-image-text-box-img-sm.php');
-	require('image-sizes.php');
+	// require('rich-text.php');
+	// require('image-columns.php');
+	// require('one-image.php');
+	// require('text-box.php');
+	// require('social-media.php');
+	// require('columns-image-text-box.php');
+	// require('columns-image-text-box-img-sm.php');
+	// require('image-sizes.php');
 ?>
