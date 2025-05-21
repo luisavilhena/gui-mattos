2f005457c837632da6a8356577d48183e03307c6
Luisa Vilhena <luisa.vilhena@base.digital>
2025-05-21 17:49:41 -0300

    update version carbon

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'luvilhena/xi-de-agosto-theme',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1bbdeb45fa40fcd14aa25e4a182fa1d7f426372d',
+        'reference' => '8b58473787ca588468bd606a2f856322c2439745',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'htmlburger/carbon-fields' => array(
-            'pretty_version' => 'v3.6.3',
-            'version' => '3.6.3.0',
-            'reference' => 'd913a5148cb9dc61ed239719c747f4ebb513003f',
+            'pretty_version' => 'v3.6.8',
+            'version' => '3.6.8.0',
+            'reference' => '6981efa8d53d9ba9a512596a462b88a6fbf4e83c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'luvilhena/xi-de-agosto-theme' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1bbdeb45fa40fcd14aa25e4a182fa1d7f426372d',
+            'reference' => '8b58473787ca588468bd606a2f856322c2439745',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
